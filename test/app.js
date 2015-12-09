9219b9684a6d71aa29b3011640e38b5f76bd5632
Bhargav Krishna <bhargav.krishna@outlook.com>
2015-12-09 13:46:57 -0800

    Updating tests

--- test/app.js
+++ test/app.js
@@ -5,15 +5,15 @@ var helpers = require('yeoman-generator').test;
 
 describe('generator-angularts:app', function () {
   before(function (done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({someOption: true})
-      .withPrompts({someAnswer: true})
+    helpers.run(path.join(__dirname, '../generators/templates'))
+      .withOptions({ 'continue': true })
+      .withPrompts({ someAnswer: true })
       .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
-      'dummyfile.txt'
+      'gulpfile.config.json'
     ]);
   });
 });
